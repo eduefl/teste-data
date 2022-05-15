c4a9f775a47f734f5bb6cea26ec14985bde76167
Eduardo Ferreira Lima <elima@npl4m.ru>
2022-05-15 19:55:09 +0300

    Concluido teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teste-data';
+  title = 'appBootstrap';
+  model: any;
+
+
+  constructor() {}
+
 }
